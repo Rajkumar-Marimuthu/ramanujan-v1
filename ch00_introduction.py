7f7ac75f8309aba5f7d499b27257e34167da37af
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-08-12 06:58:59 +0100

    update

---
+++ ch00_introduction.py
@@ -0,0 +1,22 @@
+# Python is used for web development (server side), software development, mathematics, system scripting
+# created by Guido van Rossum, and released in 1991.
+
+# Python runs on an interpreter system. This means that as a developer you write Python (.py) files
+# in a text editor and then put those files into the python interpreter to be executed.
+# to run python file, python <filename>
+# to type program in commandline type python or py
+# type exit() to quit command line
+
+# Python can be treated in a procedural way, ab oject oriented way, or a functional way
+# Latest major version is Python 3
+
+# Python uses new lines to complete a command instead of semicolon
+# instead of curly brackets
+print("Hello world")
+print(100)
+
+# Python relies on indentation, using whitespace, to define scope for loops, functions and classes,
+# The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+# You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
+if 5 > 2:
+    print("Five is greater than two!")
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-print(1)
 
 
 def say_hello(name):
